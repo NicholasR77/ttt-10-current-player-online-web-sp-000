1e3bfd7da1ad636845affc25e884eaaf55fb3141
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-14 22:26:01 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,23 +3,25 @@ test_board = ["X", "X", " ", " ", " ", " ", " ", "O", "O"]
 
 #turn count method
 def turn_count(board)
-  #create a counter
   turn_counter = 0
-  #loop through the board array
+  
   board.each do |index|
     if (index == "X" || index == "O")
       turn_counter += 1
     end
   end
-  #returns the current counter
+  
   puts turn_counter
   return turn_counter
 end
 
 #current player method
 def current_player(board)
-  if (turn_counter(board) ==
-
+  if (turn_counter(board) % 2 == 0)
+    puts "It it X player's turn."
+  elsif (turn_counter(board) % 2 != 0)
+      puts "It it O player's turn."
+  end  
 end
 
 turn_count(test_board)
