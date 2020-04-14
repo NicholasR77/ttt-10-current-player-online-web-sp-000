479717a34abc2d9c3a039de226cd68da979d8f5d
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-14 22:19:23 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 #test variables
-test_board = ["X", "X", " ", " ", " ", " ", " ", " ", " "]
+test_board = ["X", "X", " ", " ", " ", " ", " ", " O", "O"]
 
 #turn count method
 def turn_count(board)
@@ -11,7 +11,7 @@ def turn_count(board)
       turn_counter += 1
     end
   end
-  puts turn_counter
+  puts turn
 end
 
 #current player method
