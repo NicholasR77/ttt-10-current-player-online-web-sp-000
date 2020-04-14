8ef24a8a72f6fb5ac7d864ac2cf2f44b73278398
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-14 22:02:51 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,15 @@
 #turn count method
 def turn_count(board)
+  #create a counter
+  turn_counter = 0
+  #loop through the board array
 
+    #list number of turns
+    #increase counter?
+  #end loop
 end
 
 #current player method
-def current_player(arg)
+def current_player(board)
 
 end
